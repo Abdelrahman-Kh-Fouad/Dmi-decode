a8b33dee01c393fcc63172a477de3f5c50a052b7
Abdelrahman <abdokh950@gmail.com>
2021-09-19 10:39:27 +0200

    init

---
+++ DMIParser.py
@@ -0,0 +1,40 @@
+class DNIParser :
+    def __init__(self ,file :str):
+        self.content={}
+        self.fileAsLines = file.splitlines()
+        self.numberOfSpacesInIntend =self.GetNumberOfSpaces()
+
+
+    def GetNumberOfSpaces(self):
+        '''
+        Get number of spaces in one intend for parsing.
+        :return:intger -- number of spaces.
+        '''
+        for line in self.fileAsLines:
+            if len(line) - len(line.lstrip())!=0:
+                return len(line) - len(line.lstrip())
+
+
+    def GetNumberOfIntend(self , line:str):
+        '''
+        Get number of intend in a line.
+        :param line:string -- line we want search.
+        :return:intger -- number of intends
+        '''
+        return (len(line)-len(line.lstrip())) / self.numberOfSpacesInIntend
+
+    #state = noOp : 0 , sectionName : 1 , readKeyValue :2  , readList:3
+    def Parse(self , lineNumber:int , state:int ):
+        """parsing file line by line
+
+        :param lineNumber:Intger -- number of line we in in file.\n
+        :param state: Intger -- our state --> noOp : 0 , sectionName : 1 , readKeyValue :2  , readList:3.\n
+        """
+        if lineNumber == len(self.fileAsLines):
+            return
+
+        line = self.fileAsLines[lineNumber]
+        if state == 0 :
+            if lineNumber +1  < len(self.fileAsLines):
+                if self.GetNumberOfIntend(self.fileAsLines[lineNumber + 1]) > self.GetNumberOfIntend(line):
+                    self.Parse(lineNumber +1 , 1 )
